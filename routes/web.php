1872c269458fbead995d958af8b9c9a34c6e60a4
Dikdik Firman Sidik <dik2fs@gmail.com>
2025-10-15 13:20:14 +0700

    memperbaiki route

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,6 @@ use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\OrderController;
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\ProfileController as AdminProfileController;
-use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
-use App\Http\Controllers\Admin\ContactController as AdminContactController;
-use App\Http\Controllers\Admin\BookController as AdminBookController;
-use App\Http\Controllers\Admin\OrderController as AdminOrderController;
 use App\Http\Controllers\Auth\LoginController;
 
 // ==================== FRONTEND ROUTES ====================
@@ -19,7 +13,7 @@ Route::get('/', [ProfileController::class, 'index'])->name('home');
 Route::get('/about', [ProfileController::class, 'about'])->name('about');
 Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
 Route::get('/projects/{id}', [ProjectController::class, 'show'])->name('projects.show');
-Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact'); // <- DIUBAH
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
 
 // Books Routes
@@ -39,40 +33,37 @@ Route::post('/admin/logout', [LoginController::class, 'logout'])->name('admin.lo
 // ==================== ADMIN ROUTES (PROTECTED) ====================
 Route::prefix('admin')->middleware(['auth'])->group(function () {
     // Dashboard
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('admin.dashboard');
     
     // Profile Management
-    Route::get('/profile', [AdminProfileController::class, 'edit'])->name('admin.profile.edit');
-    Route::put('/profile', [AdminProfileController::class, 'update'])->name('admin.profile.update');
+    Route::get('/profile', [App\Http\Controllers\Admin\ProfileController::class, 'edit'])->name('admin.profile.edit');
+    Route::put('/profile', [App\Http\Controllers\Admin\ProfileController::class, 'update'])->name('admin.profile.update');
     
     // Projects Management
-    Route::get('/projects', [AdminProjectController::class, 'index'])->name('admin.projects.index');
-    Route::get('/projects/create', [AdminProjectController::class, 'create'])->name('admin.projects.create');
-    Route::post('/projects', [AdminProjectController::class, 'store'])->name('admin.projects.store');
-    Route::get('/projects/{project}/edit', [AdminProjectController::class, 'edit'])->name('admin.projects.edit');
-    Route::put('/projects/{project}', [AdminProjectController::class, 'update'])->name('admin.projects.update');
-    Route::delete('/projects/{project}', [AdminProjectController::class, 'destroy'])->name('admin.projects.destroy');
-    Route::put('/projects/{project}/toggle-featured', [AdminProjectController::class, 'toggleFeatured'])->name('admin.projects.toggle-featured');
+    Route::get('/projects', [App\Http\Controllers\Admin\ProjectController::class, 'index'])->name('admin.projects.index');
+    Route::get('/projects/create', [App\Http\Controllers\Admin\ProjectController::class, 'create'])->name('admin.projects.create');
+    Route::post('/projects', [App\Http\Controllers\Admin\ProjectController::class, 'store'])->name('admin.projects.store');
+    Route::get('/projects/{project}/edit', [App\Http\Controllers\Admin\ProjectController::class, 'edit'])->name('admin.projects.edit');
+    Route::put('/projects/{project}', [App\Http\Controllers\Admin\ProjectController::class, 'update'])->name('admin.projects.update');
+    Route::delete('/projects/{project}', [App\Http\Controllers\Admin\ProjectController::class, 'destroy'])->name('admin.projects.destroy');
     
     // Contacts Management
-    Route::get('/contacts', [AdminContactController::class, 'index'])->name('admin.contacts.index');
-    Route::get('/contacts/{contact}', [AdminContactController::class, 'show'])->name('admin.contacts.show');
-    Route::put('/contacts/{contact}/read', [AdminContactController::class, 'markAsRead'])->name('admin.contacts.markAsRead');
-    Route::delete('/contacts/{contact}', [AdminContactController::class, 'destroy'])->name('admin.contacts.destroy');
+    Route::get('/contacts', [App\Http\Controllers\Admin\ContactController::class, 'index'])->name('admin.contacts.index');
+    Route::get('/contacts/{contact}', [App\Http\Controllers\Admin\ContactController::class, 'show'])->name('admin.contacts.show');
+    Route::put('/contacts/{contact}/read', [App\Http\Controllers\Admin\ContactController::class, 'markAsRead'])->name('admin.contacts.markAsRead');
+    Route::delete('/contacts/{contact}', [App\Http\Controllers\Admin\ContactController::class, 'destroy'])->name('admin.contacts.destroy');
     
     // Books Management
-    Route::get('/books', [AdminBookController::class, 'index'])->name('admin.books.index');
-    Route::get('/books/create', [AdminBookController::class, 'create'])->name('admin.books.create');
-    Route::post('/books', [AdminBookController::class, 'store'])->name('admin.books.store');
-    Route::get('/books/{book}/edit', [AdminBookController::class, 'edit'])->name('admin.books.edit');
-    Route::put('/books/{book}', [AdminBookController::class, 'update'])->name('admin.books.update');
-    Route::delete('/books/{book}', [AdminBookController::class, 'destroy'])->name('admin.books.destroy');
-    Route::put('/books/{book}/toggle-featured', [AdminBookController::class, 'toggleFeatured'])->name('admin.books.toggle-featured');
-    Route::put('/books/{book}/toggle-available', [AdminBookController::class, 'toggleAvailable'])->name('admin.books.toggle-available');
+    Route::get('/books', [App\Http\Controllers\Admin\BookController::class, 'index'])->name('admin.books.index');
+    Route::get('/books/create', [App\Http\Controllers\Admin\BookController::class, 'create'])->name('admin.books.create');
+    Route::post('/books', [App\Http\Controllers\Admin\BookController::class, 'store'])->name('admin.books.store');
+    Route::get('/books/{book}/edit', [App\Http\Controllers\Admin\BookController::class, 'edit'])->name('admin.books.edit');
+    Route::put('/books/{book}', [App\Http\Controllers\Admin\BookController::class, 'update'])->name('admin.books.update');
+    Route::delete('/books/{book}', [App\Http\Controllers\Admin\BookController::class, 'destroy'])->name('admin.books.destroy');
     
     // Orders Management
-    Route::get('/orders', [AdminOrderController::class, 'index'])->name('admin.orders.index');
-    Route::get('/orders/{order}', [AdminOrderController::class, 'show'])->name('admin.orders.show');
-    Route::put('/orders/{order}/status', [AdminOrderController::class, 'updateStatus'])->name('admin.orders.updateStatus');
-    Route::delete('/orders/{order}', [AdminOrderController::class, 'destroy'])->name('admin.orders.destroy');
+    Route::get('/orders', [App\Http\Controllers\Admin\OrderController::class, 'index'])->name('admin.orders.index');
+    Route::get('/orders/{order}', [App\Http\Controllers\Admin\OrderController::class, 'show'])->name('admin.orders.show');
+    Route::put('/orders/{order}/status', [App\Http\Controllers\Admin\OrderController::class, 'updateStatus'])->name('admin.orders.updateStatus');
+    Route::delete('/orders/{order}', [App\Http\Controllers\Admin\OrderController::class, 'destroy'])->name('admin.orders.destroy');
 });
